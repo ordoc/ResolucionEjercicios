d49dd007023edefe51c9612ec31203f913a6841b
Orlando E <75282113+ordoc@users.noreply.github.com>
2023-02-13 16:48:38 -0500

    Create tema1.java

---
+++ java-basico-oe/tema1.java
@@ -0,0 +1,43 @@
+package com.tema;
+
+public class Tipos {
+
+    
+  public static void main(String[] args) {
+
+        // numericos
+
+        // 1.1. enteros
+        byte b = 10;
+        System.out.println("Esto es byte: " + b);
+        short s = 10;
+        System.out.println("Esto es un short: " + s);
+        int i = 10;
+        System.out.println("Esto es un integer: " + i);
+        long l = 100;
+        System.out.println("Esto es un long: " + l);
+        long numeroTelefono = 5555555;
+
+        //1.2. decimales
+        float f = 10.0f;
+        System.out.println("Esto es un float: " + f);
+        double variable = 10.0;
+        System.out.println("Esto es un double: " + variable);
+        double d2 = 10.0d;
+
+        double precioSilla = 49.99;
+
+        //booleanos
+        boolean var7 = true;
+        System.out.println("Esto es un boolean: " + var7);
+        boolean var8 = false;
+
+
+        // texto
+        char variable9 = 'a';
+        System.out.println("Esto es un chart: " + variable9);
+        String variable10 = "Lorem ipsum dolor sit amet...";
+        System.out.println("Esto es un string: " + variable10);
+
+    }
+}
